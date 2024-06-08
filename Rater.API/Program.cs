e249e1fb4c1198f58938f10ac6e9d18a0e3297b3
Sirmelihy <melihokutangame@hotmail.com>
2024-06-08 06:29:51 +0300

    feat(config): register AutoMapper, services, and repository in Program.cs

--- Rater.API/Program.cs
+++ Rater.API/Program.cs
@@ -1,4 +1,21 @@
+using Rater.Business.Services;
+using Rater.Business.Services.Interfaces;
+using Rater.Data.DataContext;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Rater.Data.Repositories.Interfaces;
+using Rater.Data.Repositories;
+using Rater.Business.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.IgnoreNullValues = true;
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.MaxDepth = 64; // Increase the depth if needed
+});
 
 // Add services to the container.
 
@@ -6,6 +23,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DBBContext>();
+
+
+builder.Services.AddScoped<ISpaceRepository, SpaceRepository>();
+builder.Services.AddScoped<ISpaceService, SpaceService>();
+
+builder.Services.AddScoped<IMetricRepository, MetricRepository>();
+builder.Services.AddScoped<IMetricService, MetricService>();
+
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
 
 var app = builder.Build();
 
