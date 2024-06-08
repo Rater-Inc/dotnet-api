f3e78fe568325e6909bb4afb04f5c684f2c0a2c0
Sirmelihy <melihokutangame@hotmail.com>
2024-06-08 06:28:09 +0300

    feat(space): add MetricController, MetricService, MetriceRepository, and DTOs

---
+++ Rater.API/Controllers/MetricController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rater.Business.Services.Interfaces;
+
+namespace Rater.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetricController : ControllerBase
+    {
+
+        IMetricService _service;
+        public MetricController(IMetricService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetAllMetrics")]
+        public async Task<ActionResult<List<Metric>>> GetAllMetrics ()
+        {
+            var value = await _service.GetMetrics();
+            return Ok(value);
+        }
+    }
+}
---
+++ Rater.Business/Services/MetricService.cs
@@ -0,0 +1,28 @@
+﻿using Rater.API;
+using Rater.Business.Services.Interfaces;
+using Rater.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rater.Business.Services
+{
+    public class MetricService : IMetricService
+    {
+
+        private readonly IMetricRepository _repo;
+        public MetricService(IMetricRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        public async Task<List<Metric>> GetMetrics()
+        {
+            var value = await _repo.GetAllMetrics();
+            return value;
+        }
+    }
+}
