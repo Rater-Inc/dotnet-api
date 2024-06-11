0b5a2be51e015d3aece47d842334e58191b01359
Melih <58309701+Sirmelihy@users.noreply.github.com>
2024-06-11 22:41:59 +0300

    refactor(di): register service interface for dependency injection

--- Rater.API/Program.cs
+++ Rater.API/Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IParticipantRepository, ParticipantRepository>();
 builder.Services.AddScoped<IParticipantService, ParticipantService>();
 
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
 
