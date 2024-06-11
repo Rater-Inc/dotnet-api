6c03d69dc241c73199ab51c53f028f864909cabb
Melih <58309701+Sirmelihy@users.noreply.github.com>
2024-06-11 22:37:03 +0300

    feat(authentication): add authentication functionality
    - Implement authentication service
    - Add JWT token generation

---
+++ Rater.Business/Services/AuthService.cs
@@ -0,0 +1,73 @@
+﻿using BCrypt.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Rater.Business.Services.Interfaces;
+using Rater.Data.Repositories.Interfaces;
+using Rater.Domain.DataTransferObjects.AuthDto;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Rater.Business.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ISpaceRepository _spaceRepository;
+        private readonly IConfiguration _configuration;
+        public AuthService(ISpaceRepository spaceRepository,IConfiguration configuration)
+        {
+            _spaceRepository = spaceRepository;
+            _configuration = configuration;
+        }
+
+
+        public async Task<AuthResponseDto> AuthLobby (string link , string password)
+        {
+
+            try
+            {
+                AuthResponseDto response = new AuthResponseDto();
+                var space = await _spaceRepository.GetSpaceByLink(link);
+                if(BCrypt.Net.BCrypt.Verify(password,space.Password))
+                {
+                    response.Success = true;
+                    response.jwtToken = CreateToken();
+                    return response;
+                }
+                return response;
+
+            }
+            catch (Exception ex){
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public string CreateToken()
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "DEFAULT"),
+                new Claim(ClaimTypes.Role, "user")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("jwt:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(24),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+
+        }
+
+
+    }
+}
---
+++ Rater.Domain/DataTransferObjects/AuthDto/AuthResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rater.Domain.DataTransferObjects.AuthDto
+{
+    public class AuthResponseDto
+    {
+        public bool Success { get; set; } = false;
+        public string jwtToken { get; set; } = string.Empty;
+    }
+}
