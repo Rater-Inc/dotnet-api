eb762793d0805b12a27b17b6912bf50cda8ad59a
Melih <58309701+Sirmelihy@users.noreply.github.com>
2024-07-31 23:32:48 +0300

    feat: Add MemoryCache using decorator pattern

---
+++ Rater.Data/Repositories/Decorator/CachedMetricRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Rater.API;
+using Rater.Data.Repositories.Interfaces;
+using Rater.Domain.DataTransferObjects.MetricDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rater.Data.Repositories.Decorator
+{
+    public class CachedMetricRepository : IMetricRepository
+    {
+        private readonly MetricRepository _decorated;
+        private readonly IMemoryCache _memoryCache;
+        public CachedMetricRepository(MetricRepository decorated , IMemoryCache memoryCache)
+        {
+            _decorated = decorated;
+            _memoryCache = memoryCache;
+        }
+
+        public Task<List<MetricResponseDto>> CreateMetrics(List<MetricRequestDto> request)
+        {
+            return _decorated.CreateMetrics(request);
+        }
+
+        public async Task<List<Metric>> GetAllMetrics(int space_id)
+        {
+            string key = $"metric{space_id}";
+            var cachedMetrics = await _memoryCache.GetOrCreateAsync(
+                key,
+                async entry =>
+                {
+                    entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(10));
+                    return await _decorated.GetAllMetrics(space_id);
+                });
+            return cachedMetrics == null ? throw new Exception("Failed to retrieve Space") : cachedMetrics;
+        }
+    }
+}
---
+++ Rater.Data/Repositories/Decorator/CachedSpaceRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Rater.API;
+using Rater.Data.Repositories.Interfaces;
+using Rater.Domain.DataTransferObjects.SpaceDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rater.Data.Repositories.Decorator
+{
+    public class CachedSpaceRepository : ISpaceRepository
+    {
+        private readonly SpaceRepository _decorated;
+        private readonly IMemoryCache _memoryCache;
+        public CachedSpaceRepository(SpaceRepository decorated,IMemoryCache memoryCache)
+        {
+            _decorated = decorated;
+            _memoryCache = memoryCache;
+        }
+        public async Task<SpaceResponseDto> CreateSpace(SpaceRequestDto request)
+        {
+            return await _decorated.CreateSpace(request);
+        }
+
+        public async Task<Space> GetSpaceByLink(string link)
+        {
+            string key = $"space{link}";
+            var cachedSpace = await _memoryCache.GetOrCreateAsync(
+                key,
+                async entry =>
+                {
+                    entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(10));
+                    return await _decorated.GetSpaceByLink(link);
+                });
+            return cachedSpace == null ? throw new Exception("Failed to retrieve Space") : cachedSpace;
+        }
+
+        public async Task<bool> SpaceExist(int space_id)
+        {
+            string key = $"space:exists:{space_id}";
+            var cachedSpaceExists = await _memoryCache.GetOrCreateAsync(
+                key,
+                async entry =>
+                {
+                    entry.SetAbsoluteExpiration(TimeSpan.FromHours(1));
+                    return await _decorated.SpaceExist(space_id);
+                });
+            return cachedSpaceExists;
+        }
+    }
+}
